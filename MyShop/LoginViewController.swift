0d4ec0d9170b0f1932388130c7039ec269e2576d
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2023-08-11 09:41:34 +1000

    login and Registration UI completed

---
+++ MyShop/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  MyShop
+//
+//  Created by Anup Kuriakose on 11/8/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+   
+}
--- MyShop/ProductDetailViewController.swift
+++ MyShop/ProductDetailViewController.swift
@@ -23,9 +23,23 @@ class ProductDetailViewController: UIViewController {
     @IBOutlet weak var addCartBtn: UIButton!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
 
+            // Add a target to the addCartBtn to call the addItemToCart method when clicked
+            addCartBtn.addTarget(self, action: #selector(addItemToCart), for: .touchUpInside)
+        }
+
+        @objc func addItemToCart() {
+            // Create an alert
+            let alert = UIAlertController(title: "Item Added to Cart:", message: "iPhone Pro Max", preferredStyle: .alert)
+            
+            // Add an "OK" button to the alert
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            // Present the alert
+            self.present(alert, animated: true, completion: nil)
+        }
         
     }
-    
-}
+
+
