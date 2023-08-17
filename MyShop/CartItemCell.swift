b1e344080093edcd4b2df1e8387b4d3e30f68dd0
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2023-08-17 12:30:55 +1000

    Cart Item cells sizes and layout modified

--- MyShop/CartItemCell.swift
+++ MyShop/CartItemCell.swift
@@ -1,23 +1,33 @@
-//
-//  CartItemCell.swift
-//  MyShop
-//
-//  Created by Anup Kuriakose on 10/8/2023.
-//
+
 
 import UIKit
 
 class CartItemCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var cartItemImage: UIImageView!
+    
+    @IBOutlet weak var cartItemLbl: UILabel!
+    
+    @IBOutlet weak var CartItemPrice: UILabel!
+    
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+            super.awakeFromNib()
+            configureCell()
+        }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+        }
+        
+        func configureCell() {
+            cartItemImage.image = UIImage(named: "laptop")
+            cartItemLbl.text = "HP Laptop"
+            CartItemPrice.text = "A$ 1000"
+        }
     }
     
-}
+
+
+
+
